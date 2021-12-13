50ffc098c4e0013e20efb8c8343d337ae3cdc3d4
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-12-13 17:17:33 -0500

    Prueba 3

--- src/main/java/com/reto2/controller/GadgetController.java
+++ src/main/java/com/reto2/controller/GadgetController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/cosmetics/")
+@RequestMapping("/api/cosmetics")
 @CrossOrigin("*")
 public class GadgetController {
     @Autowired
