2175f2946c6e123c6f7a030fa1faf3f13caf3c3b
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-12-14 12:07:02 -0500

    Prueba 4

--- src/main/java/com/reto2/interfaces/GadgetInterface.java
+++
@@ -1,8 +0,0 @@
-package com.reto2.interfaces;
-
-import com.reto2.model.Gadget;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GadgetInterface extends MongoRepository<Gadget, String> {
-
-}
